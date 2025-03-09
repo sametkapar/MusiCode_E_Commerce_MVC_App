e35a223ef8f52b241d427df85a5473f0c2e382e1
alpsarikisla <50963484+alpsarikisla@users.noreply.github.com>
2025-03-09 14:22:03 +0300

    ürün düzenleme tamamlandı

--- MusiCodeWebApp/Areas/ManagerPanel/Controllers/ProductController.cs
+++ MusiCodeWebApp/Areas/ManagerPanel/Controllers/ProductController.cs
@@ -81,46 +81,101 @@ namespace MusiCodeWebApp.Areas.ManagerPanel.Controllers
         }
 
         // GET: ManagerPanel/Product/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if (id != null)
+            {
+                Product p = db.Products.Find(id);
+                if (p != null)
+                {
+                    if (!p.IsDeleted)
+                    {
+                        ViewBag.Category_ID = new SelectList(db.Categories.Where(x => x.IsDeleted == false), "ID", "Name",p.Category_ID);
+                        ViewBag.Brand_ID = new SelectList(db.Brands.Where(x => x.IsDeleted == false), "ID", "Name", p.Brand_ID);
+                        return View(p);
+                    }
+                    else
+                    {
+                        TempData["systemerror"] = "Ürün silinmiş";
+                        return RedirectToAction("Error", "System");
+                    }
+                }
+                else
+                {
+                    TempData["systemerror"] = "Ürün Bulunamadı";
+                    return RedirectToAction("Error", "System");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Product");
+            }
         }
 
         // POST: ManagerPanel/Product/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(Product Model, HttpPostedFileBase image)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
+                try
+                {
+                    bool isvalidimage = true;
+                    if (image != null)
+                    {
+                        FileInfo fi = new FileInfo(image.FileName);
+                        string extension = fi.Extension;
+                        if (extension == ".jpg" || extension == ".jpeg" || extension == ".png")
+                        {
+                            string name = Guid.NewGuid().ToString() + extension;
+                            Model.Image = name;
+                            image.SaveAs(Server.MapPath("~/Assets/ProductImages/" + name));
+                        }
+                        else
+                        {
+                            isvalidimage = false;
+                            ViewBag.mesaj = "Resim uzantısı .jpg, .jpeg, .png olabilir";
+                        }
+                    }
+                    if (isvalidimage)
+                    {
+                        db.Entry(Model).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        TempData["mesaj"] = "Ürün Düzenleme başarılı";
+                        return RedirectToAction("Index", "Product");
+                    }
+                }
+                catch
+                {
+                    ViewBag.mesaj = "Ürün düzenlenirken bir hata oluştu";
+                }
             }
-            catch
-            {
-                return View();
-            }
+            return View(Model);
+          
         }
-
-        // GET: ManagerPanel/Product/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int? id)
         {
-            return View();
-        }
-
-        // POST: ManagerPanel/Product/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
+            if (id != null)
             {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
+                Product p = db.Products.Find(id);
+                if (p != null)
+                {
+                    p.IsDeleted = true;
+                    p.IsActive = false;
+                    p.IsRecent = false;
+                    db.SaveChanges();
+                    TempData["mesaj"] = "Ürün silindi";
+                    return RedirectToAction("Index", "Product");
+                }
+                else
+                {
+                    TempData["systemerror"] = "Ürün Bulunamadı";
+                    return RedirectToAction("Error", "System");
+                }
             }
-            catch
+            else
             {
-                return View();
+                return RedirectToAction("Index", "Product");
             }
         }
     }
